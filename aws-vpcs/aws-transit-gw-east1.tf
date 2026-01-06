03b39543a8d2de715a4d62d6572463648de7e8b4
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-06 15:29:39 -0600

    tgw route

--- aws-vpcs/aws-transit-gw-east1.tf
+++ aws-vpcs/aws-transit-gw-east1.tf
@@ -19,3 +19,9 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_east1" {
     Name = "vpc1-east1-attachment"
   }
 }
+
+resource "aws_ec2_transit_gateway_route" "to_east2_tgw" {
+  destination_cidr_block         = [ "10.9.0.0/21" ] # East-2 CIDR's
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.us_east1_hub.propagation_default_route_table_id
+}
