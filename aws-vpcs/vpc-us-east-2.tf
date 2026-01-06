e3bf29492d619cb760c08c97093b46afdff83c77
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-06 10:45:33 -0600

    alias

--- aws-vpcs/vpc-us-east-2.tf
+++ aws-vpcs/vpc-us-east-2.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "ohio_vpc1" {
   cidr_block       = "10.9.0.0/21"
   instance_tenancy = "default"
-  provider = "us-east-2"
+  provider = aws.us-east-2
 
   tags = {
     Name = "vpc1-ohio"
