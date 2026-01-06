bb005ad6c78be7706c7154dc369551446e43d42e
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-06 14:27:57 -0600

    transit gateway deployment

---
+++ aws-vpcs/aws-transit-gw.tf
@@ -0,0 +1,32 @@
+resource "aws_ec2_transit_gateway" "us_hub" {
+  description = "US Hub for VPC peering"
+  
+  auto_accept_shared_attachments = "enable"
+  default_route_table_association = "enable"
+  default_route_table_propagation = "enable"
+
+  tags = {
+    Name = "us-hub-tgw"
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_east1" {
+  subnet_ids         = ["subnet-0fb212c06357f4d21", "subnet-09ce0e87f2c573a7c"] 
+  transit_gateway_id = aws_ec2_transit_gateway.us_hub.id
+  vpc_id             = "vpc-01bb5a7a020da430f"
+
+  tags = {
+    Name = "vpc1-east1-attachment"
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_east2" {
+  subnet_ids         = ["subnet-073045df8b055adfa", "subnet-03330ce9956b684e0"] 
+  transit_gateway_id = aws_ec2_transit_gateway.us_hub.id
+  vpc_id             = "vpc-0c7804067cd0ddef4"
+  provider = aws.us-east-2
+
+  tags = {
+    Name = "vpc1-east2-attachment"
+  }
+}
