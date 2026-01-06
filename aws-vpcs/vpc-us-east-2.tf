0140495677fb0177665a0f3818e1ff03788bdbc1
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-06 14:17:52 -0600

    ohio subnets

--- aws-vpcs/vpc-us-east-2.tf
+++ aws-vpcs/vpc-us-east-2.tf
@@ -11,6 +11,7 @@ resource "aws_vpc" "ohio_vpc1" {
 resource "aws_subnet" "ohio_vpc1_sn1" {
   vpc_id     = aws_vpc.ohio_vpc1.id
   cidr_block = "10.9.0.0/24"
+  provider = aws.us-east-2
 
   tags = {
     Name = "ohio_vpc1_sn1"
@@ -20,6 +21,7 @@ resource "aws_subnet" "ohio_vpc1_sn1" {
 resource "aws_subnet" "ohio_vpc1_sn2" {
   vpc_id     = aws_vpc.ohio_vpc1.id
   cidr_block = "10.9.1.0/24"
+  provider = aws.us-east-2
 
   tags = {
     Name = "ohio_vpc1_sn2"
