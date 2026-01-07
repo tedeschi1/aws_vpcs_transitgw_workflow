5054599f22678f02c9caf6c2a0b869950d794a85
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-07 10:32:48 -0600

    output

--- aws-vpcs/aws-transit-gw-east1.tf
+++ aws-vpcs/aws-transit-gw-east1.tf
@@ -11,7 +11,7 @@ resource "aws_ec2_transit_gateway" "us_east1_hub" {
 }
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_east1" {
-  subnet_ids         = [ aws_subnet.virginia_vpc1_sn1, aws_subnet.virginia_vpc1_sn2 ] 
+  subnet_ids         = [ aws_subnet.virginia_vpc1_sn1.id, aws_subnet.virginia_vpc1_sn2.id ] 
   transit_gateway_id = aws_ec2_transit_gateway.us_east1_hub.id
   vpc_id             = aws_vpc.virginia_vpc1.id
 
@@ -23,5 +23,9 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_east1" {
 resource "aws_ec2_transit_gateway_route" "to_east2_tgw" {
   destination_cidr_block         = "10.9.0.0/21" # East-2 CIDR's
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway.us_east1_hub.propagation_default_route_table_id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.us_east1_hub.propagation_default_route_table_id.id
+}
+
+output "aws_ec2_tgw_route_table_id" {
+  value = aws_ec2_transit_gateway.us_east1_hub.propagation_default_route_table_id.id
 }
