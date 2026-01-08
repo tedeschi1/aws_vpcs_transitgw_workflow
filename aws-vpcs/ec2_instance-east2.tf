353061d6b186b2262081de369e87f3c2ab7ae0fe
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-08 08:35:19 -0600

    add east2 ec2 provider

--- aws-vpcs/ec2_instance-east2.tf
+++ aws-vpcs/ec2_instance-east2.tf
@@ -1,6 +1,7 @@
 data "aws_ami" "linux_ec2_east_2" {
   most_recent = true
   owners      = ["amazon"]
+  provider      = aws.us-east-2
 
   filter {
     name   = "name"
@@ -11,11 +12,12 @@ data "aws_ami" "linux_ec2_east_2" {
 resource "aws_instance" "east_2_ec2" {
   ami           = data.aws_ami.linux_ec2_east_2.id
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.ohio_vpc1_sn2.id
+  subnet_id     = aws_subnet.ohio_vpc1_sn1.id
+  provider      = aws.us-east-2
 
   tags = {
     Name = "ec2-east2"
   }
 
-  depends_on = [aws_subnet.ohio_vpc1_sn2]
+  depends_on = [aws_subnet.ohio_vpc1_sn1]
 }
