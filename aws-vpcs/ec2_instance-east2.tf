fa99c4936172f5e48e05149fc22046fefd8dc392
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-08 08:30:27 -0600

    east2 ec2

--- aws-vpcs/ec2_instance-east2.tf
+++ aws-vpcs/ec2_instance-east2.tf
@@ -11,11 +11,11 @@ data "aws_ami" "linux_ec2_east_2" {
 resource "aws_instance" "east_2_ec2" {
   ami           = data.aws_ami.linux_ec2_east_2.id
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.ohio_vpc1_sn1.id
+  subnet_id     = aws_subnet.ohio_vpc1_sn2.id
 
   tags = {
     Name = "ec2-east2"
   }
 
-  depends_on = [aws_subnet.ohio_vpc1_sn1]
+  depends_on = [aws_subnet.ohio_vpc1_sn2]
 }
